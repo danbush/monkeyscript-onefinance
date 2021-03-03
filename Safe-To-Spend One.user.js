9e03eab5c48f69bd4f9b3046be5d40e6c167e271
Dan Bush <dan@danbush.me>
2021-03-03 11:22:44 -0600

    Update Safe-To-Spend One.user.js
    fixed a class call that was modified on One's end.

--- Safe-To-Spend One.user.js
+++ Safe-To-Spend One.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Safe-To-Spend One
 // @namespace    http://tampermonkey.net/
-// @version      0.51
+// @version      0.53
 // @description  Hide the total cash number, dark mode, more.
 // @author       Dan Bush
 // @match        https://web.onefinance.com/*
@@ -21,7 +21,8 @@
 
     addGlobalStyle('.StyledHeader-sc-1zehgg.Fexki, button[data-cy="bridge-summary"] {display:none !important;}');
     addGlobalStyle('button[aria-label="Spend pocket details"] div {font-size: 30px !important;}');
-    //addGlobalStyle('body { filter: invert(90%) hue-rotate(180deg) !important;}');
+    addGlobalStyle('button[aria-label="Spend pocket details"] div.PocketContentDiv-sc-1kwy879.spending.StyledPocketTitleDiv-sc-1x61pf6:before {content: "Safe to " !important;}');
+
     addGlobalStyle('html { filter: invert(90%) hue-rotate(180deg) !important;}');
     addGlobalStyle('body img {filter: invert(90%) hue-rotate(180deg) contrast(1.01) saturate(1.03) brightness(1.02) !important;}');
     addGlobalStyle('.StyledPocketsLi-sc-17s7uta.eLbUJd {filter: invert(90%) hue-rotate(180deg) contrast(1.01) saturate(1.03) brightness(1.02) !important;}');
@@ -32,7 +33,7 @@
     addGlobalStyle('svg, .StyledLogoDiv-sc-1w3h60o {filter: invert(1) hue-rotate(180deg) !important;}');
     addGlobalStyle('div[data-cy="activity-message"] {background-color: #888888 !important;}');
     addGlobalStyle('.StyledSection-sc-u4z415 svg {filter: invert(0) hue-rotate(0deg) !important;}');
-    addGlobalStyle('button[aria-label="Spend pocket details"] div.PocketContentDiv-sc-1kwy879.spending.StyledPocketTitleDiv-sc-1x61pf6.ksNVKf:before {content: "Safe to " !important;}');
     addGlobalStyle('.iDfcUd:not(:disabled).focused,.ewoOwM:not(:disabled).focused {background: linear-gradient(rgb(255, 255, 255) 93%, rgb(3 199 0) 95%);}');
     addGlobalStyle('.iDfcUd:not(:disabled):hover, .iDfcUd:not(:disabled):focus,.ewoOwM:not(:disabled):hover,.ewoOwM:not(:disabled):focus {border: 1px solid rgb(3 199 0);}');
+
 })();
